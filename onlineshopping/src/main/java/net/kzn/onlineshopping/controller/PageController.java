c8e4a4dac32f995d4da7d9513bc461c6b817b42a
Jatin Dobariya <jatinrd28@gmail.com>
2018-05-01 09:15:50 +0530

    Complete successfully

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -8,35 +8,37 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
-	
-	@RequestMapping(value = {"/","/home","/index"})
-	public ModelAndView index(){
-		
-		ModelAndView mv= new ModelAndView("page");
-		mv.addObject("greeting","welcome to spring web mvc");
+
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "welcome to spring web mvc");
 		return mv;
-					
+
 	}
-	
-//	@RequestMapping(value = "/test")
-//	public ModelAndView test(@RequestParam(value="greeting", required=false) String greeting){
-//		if(greeting==null){
-//			greeting = "hello there";
-//		}
-//		ModelAndView mv= new ModelAndView("page");
-//		mv.addObject("greeting",greeting);
-//		return mv;
-//					
-//	}
-	
-//	@RequestMapping(value = "/test/{greeting}")
-//	public ModelAndView test(@PathVariable(value="greeting") String greeting){
-//		if(greeting==null){
-//			greeting = "hello there";
-//		}
-//		ModelAndView mv= new ModelAndView("page");
-//		mv.addObject("greeting",greeting);
-//		return mv;
-//	}
+
+	@RequestMapping(value = "/test")
+	public ModelAndView test(
+			@RequestParam(value = "greeting", required = false) String greeting) {
+		if (greeting == null) {
+			greeting = "hello there";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+
+	}
+
+	// @RequestMapping(value = "/test/{greeting}")
+	// public ModelAndView test(@PathVariable(value="greeting") String
+	// greeting){
+	// if(greeting==null){
+	// greeting = "hello there";
+	// }
+	// ModelAndView mv= new ModelAndView("page");
+	// mv.addObject("greeting",greeting);
+	// return mv;
+	// }
 
 }
